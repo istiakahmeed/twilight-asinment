ac2d83cf695c3dffd46b647ab3f1769320cd40c4
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-06-07 09:50:45 +0600

    🤯end

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import useCart from "@/hooks/useCart";
-import { CartItem } from "@/types/type";
 
 import SectionContainer from "@/utils/SectionContainer";
 
@@ -10,60 +9,9 @@ const Cart = () => {
 
   return (
     <SectionContainer>
-      <div className="cart-container">
-        <h2 className="text-2xl font-semibold mb-4">Your Cart</h2>
-        <p>Total Items in Cart: {getCartQuantity()}</p>
-
-        <ul>
-          {cart.map((item: CartItem) => (
-            <li
-              key={item.id}
-              className="flex justify-between items-center mb-2"
-            >
-              <div className="flex items-center">
-                <img
-                  src={item.imageSrc}
-                  alt={item.title}
-                  className="w-20 h-20 mr-4"
-                />
-                <div>
-                  <h3 className="font-semibold">{item.title}</h3>
-                  <p>Price: ৳{item.price}</p>
-                  <p>Quantity: {item.quantity}</p>
-                </div>
-              </div>
-              <div className="flex items-center">
-                <button
-                  onClick={() => updateQuantity(item.id, item.quantity - 1)}
-                  disabled={item.quantity <= 1}
-                >
-                  -
-                </button>
-                <button
-                  onClick={() => updateQuantity(item.id, item.quantity + 1)}
-                >
-                  +
-                </button>
-                <button
-                  onClick={() => removeFromCart(item.id)}
-                  className="ml-4"
-                >
-                  Remove
-                </button>
-              </div>
-            </li>
-          ))}
-        </ul>
-
-        {cart.length > 0 && (
-          <button
-            onClick={() => clearCart()}
-            className="mt-4 bg-red-500 text-white py-2 px-4 rounded"
-          >
-            Clear Cart
-          </button>
-        )}
-      </div>
+      <h1 className="text-center text-4xl font-bold pt-20">
+        I Think i could create this😊
+      </h1>
     </SectionContainer>
   );
 };
--- src/components/navbar/BottomNav.tsx
+++ src/components/navbar/BottomNav.tsx
@@ -1,4 +1,5 @@
 import { Search, ShoppingBag } from "lucide-react";
+import Link from "next/link";
 
 const BottomNav = () => {
   return (
@@ -16,7 +17,10 @@ const BottomNav = () => {
           >
             <path d="M11.03 2.59a1.501 1.501 0 0 1 1.94 0l7.5 6.363a1.5 1.5 0 0 1 .53 1.144V19.5a1.5 1.5 0 0 1-1.5 1.5h-5.75a.75.75 0 0 1-.75-.75V14h-2v6.25a.75.75 0 0 1-.75.75H4.5A1.5 1.5 0 0 1 3 19.5v-9.403c0-.44.194-.859.53-1.144ZM12 3.734l-7.5 6.363V19.5h5v-6.25a.75.75 0 0 1 .75-.75h3.5a.75.75 0 0 1 .75.75v6.25h5v-9.403Z"></path>
           </svg>
-          <span className="tab tab-home block text-xs">Home</span>
+          <Link href="/">
+            {" "}
+            <span className="tab tab-home block text-xs">Home</span>
+          </Link>
         </div>
         <div className="w-full focus:text-teal-500 flex items-center flex-col hover:text-teal-500 justify-center text-center pt-2 pb-1">
           <svg
@@ -39,7 +43,9 @@ const BottomNav = () => {
 
         <div className="w-full focus:text-teal-500 flex items-center flex-col hover:text-teal-500 justify-center text-center pt-2 pb-1">
           <ShoppingBag size={24} />
-          <span className="tab tab-home block text-xs">Cart</span>
+          <Link href="/cart">
+            <span className="tab tab-home block text-xs">Cart</span>
+          </Link>
         </div>
       </div>
     </nav>
